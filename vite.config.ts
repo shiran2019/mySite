5cd68b49f7f82c3f5ce67726c89c906b44eddf7e
NavodyaWeerakkody <shiran@zdatai.com>
2026-04-21 23:27:20 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     TanStackRouterVite(),
     react(),
     tsConfigPaths(),
   ],
-  base: '/mySite/',
+  base: '/',
   build: {
     target: 'es2020',
     outDir: 'dist',
