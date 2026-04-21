ab96e46b36422bd7e8571a91a7da8b0bdeb1477b
NavodyaWeerakkody <shiran@zdatai.com>
2026-04-21 23:09:36 +0530

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,4 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
-import tsConfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-  plugins: [
-    TanStackRouterVite(),
-    react(),
-    tsConfigPaths(),
-  ],
-  base: '/',
-  server: {
-    middlewareMode: true,
-  },
-});
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+
+export default defineConfig();
